c9e68926409c5ae748aa929429d736a72a598b85
Liwenroul <2728608191@qq.com>
2019-11-07 15:54:41 +0800

    duty

---
+++ build/precache-manifest.ab1439225e87eb22ca7860af78b25781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b8b483bd52f52bab7b450f3b5cbc95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c07246a15611c6bbc4ea",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "672e1bfabe49a3846818",
+    "url": "/static/css/main.81accfc2.chunk.css"
+  },
+  {
+    "revision": "c07246a15611c6bbc4ea",
+    "url": "/static/js/2.0caffaf5.chunk.js"
+  },
+  {
+    "revision": "672e1bfabe49a3846818",
+    "url": "/static/js/main.1017baab.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
